00f5740565d60c0105849e2cc64be00aba46b975
Volodymyr Cherevchuk <cherevchuk.v@gmail.com>
2025-11-01 19:22:58 +0200

    update package name

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
-      name: "is-toolkit",
+      name: "check-toolkit",
       formats: ["es"],
     },
   },
